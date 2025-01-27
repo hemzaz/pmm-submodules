0c1cbd4efc2e349d1782c52e03eab7bed07c5976
Alex Demidoff <alex@demidoff.me>
2025-01-27 17:20:30 +0300

    PMM-12153 fix get_repo() failure with '.git' suffix

--- ci.py
+++ ci.py
@@ -232,6 +232,8 @@ class Builder():
             else:
                 target_url = next(item for item in self.config['deps'] if item["name"] == dep['name'])['url']
             repo_path = '/'.join(target_url.split('/')[-2:])
+            if '.git' in repo_path:
+                repo_path = repo_path.replace('.git', '')
             target_branch = dep['branch']
             repo = github_api.get_repo(repo_path)
             org = repo.organization.name if repo.organization else repo.owner.login
